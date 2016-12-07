631ca5dc3c4bbc8301af186d4f7c46e5453199cd
zharko <nadezhda.zharko@gmail.com>
2016-12-07 12:45:06 +0300

    Seeds setup

---
+++ spec/factories/notes.rb
@@ -0,0 +1,10 @@
+require 'faker'
+
+FactoryGirl.define do
+  factory :note do
+    title {Faker::Book.title}
+    content {Faker::Hacker.say_something_smart}
+    appointment {Faker::Time.forward(10, :all)}
+    user_id {rand(29)}
+  end
+end
